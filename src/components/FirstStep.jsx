813d34ff2699375d4c5f6aaf278ce37c7b6ef37a
Dario Hunjadi <dario.hunjadi@hotmail.com>
2021-10-03 18:48:42 +0200

    last error message added

--- src/components/FirstStep.jsx
+++ src/components/FirstStep.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AnswersContext } from "../context/AnswersContext";
 import { PageContext } from "../context/PageContext";
 import pageList from "../pageList";
@@ -7,6 +7,7 @@ import { v4 as uuidv4 } from "uuid";
 export default function FirstStep() {
   const { page, setPage } = useContext(PageContext);
   const { radio, setRadio } = useContext(AnswersContext);
+  const [ error, setError ] = useState(false)
 
   const inputs = pageList[page].answers.map((ans) => {
     return (
@@ -30,6 +31,10 @@ export default function FirstStep() {
     if (radio) {
       setPage((prev) => prev + 1);
     }
+
+    if(!radio) {
+      setError(!error)
+    }
   };
 
   return (
@@ -38,6 +43,7 @@ export default function FirstStep() {
         <h1>{pageList[page].text}</h1>
       </div>
       <div className="inputs">{inputs}</div>
+      {error && <p className='error-msg'>Odaberite Proizvođača</p>}
       <div className="btn-container">
         <button className="next-btn" onClick={handleNext}>
           Dalje
